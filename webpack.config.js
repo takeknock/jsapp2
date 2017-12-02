78ec524d71d4f10bc68487b7327aa14739d45aca
mmatthew <takeknock@gmail.com>
2017-12-02 23:03:35 +0900

    update to be adapted for eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,53 @@
-var publicDir = __dirname + '/public';
-module.exports = {
-  entry : [
-    './src/index.js'
-  ],
-  output: {
-    path: publicDir,
-    publicPath: '/',
-    filename: 'bundle.js'
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const publicDir = path.join(__dirname, '/public');
+module.exports = [
+  {
+    entry: [
+      './src/index.js',
+    ],
+    output: {
+      path: publicDir,
+      publicPath: '/',
+      filename: 'bundle.js',
+    },
+    module: {
+      loaders: [{
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      }],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+      historyApiFallback: true,
+      contentBase: publicDir,
+    },
   },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015']
-      }
-    }]
+  {
+    entry: {
+      style: './stylesheets/index.scss',
+    },
+    output: {
+      path: publicDir,
+      publicPath: '/',
+      filename: 'bundle.css',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }),
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('bundle.css'),
+    ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: publicDir
-  }
-};
+];
